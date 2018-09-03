2d543d8a248a2b6789f377fe1e8388ff1d76ca84
liangxin1300 <XLiang@suse.com>
2018-09-03 14:02:57 +0800

    dev: Add api/v1/nodes/{nodename} handler

--- api_nodes.go
+++ api_nodes.go
@@ -7,60 +7,65 @@ import (
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+	"strings"
 )
 
-type Result struct {
-	NodeItem struct {
-		InnerXML string `xml:",innerxml"`
-	} `xml:"configuration>nodes"`
-}
-
-type Nodes struct {
-	NodesList []Node `xml:"node" json:"nodes"`
-}
-
-type Node struct {
-	Id    string `xml:"id,attr" json:"id"`
-	Uname string `xml:"uname,attr" json:"uname"`
-}
-
-func handleApiNodes(version string, w http.ResponseWriter, cib_data string) bool {
-	m := map[string]func(http.ResponseWriter, string)bool{
+func handleApiNodes(version string, w http.ResponseWriter, r *http.Request, cib_data string) bool {
+	m := map[string]func(http.ResponseWriter, *http.Request, string) bool{
 		"v1": handleApiNodesV1,
 		"v2": handleApiNodesV2,
 	}
 
-	return m[version](w, cib_data)
+	return m[version](w, r, cib_data)
 }
 
-func handleApiNodesV1(w http.ResponseWriter, cib_data string) bool {
-	xmlData := []byte(cib_data)
-	var result Result
-	err := xml.Unmarshal(xmlData, &result)
+func handleApiNodesV1(w http.ResponseWriter, r *http.Request, cib_data string) bool {
+	// parse xml into Cib struct
+	var cib Cib
+	err := xml.Unmarshal([]byte(cib_data), &cib)
 	if err != nil {
 		log.Error(err)
 		return false
 	}
 
-	nodesXML := []byte(fmt.Sprintf("<nodes>%s</nodes>", result.NodeItem.InnerXML))
-	var nodes Nodes
-	err = xml.Unmarshal(nodesXML, &nodes)
-	if err != nil {
-		log.Error(err)
-		return false
-	}
+	cib.Configuration.URLType = "nodes"
 
 	w.Header().Set("Content-Type", "application/json")
-	jsonData, jsonError := json.Marshal(nodes)
+	urllist := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	if len(urllist) == 3 {
+		// for url api/v[1-9]/nodes
+		cib.Configuration.Nodes.URLType = "all"
+	} else {
+		// for url api/v[1-9]/nodes/{nodeid}
+		cib.Configuration.Nodes.URLType = "node"
+
+		nodeIndex := urllist[3]
+		var index int = -1
+		for i, item := range cib.Configuration.Nodes.Node {
+			if nodeIndex == item.Uname || nodeIndex == item.Id {
+				index = i
+				break
+			}
+		}
+		if index == -1 {
+			http.Error(w, fmt.Sprintf("No route for %v.", r.URL.Path), 500)
+			return false
+		}
+
+		cib.Configuration.Nodes.URLIndex = index
+	}
+
+	jsonData, jsonError := json.Marshal(&cib)
 	if jsonError != nil {
 		log.Error(jsonError)
 		return false
 	}
-	io.WriteString(w, string(jsonData))
+
+	io.WriteString(w, string(jsonData)+"\n")
 	return true
 }
 
-func handleApiNodesV2(w http.ResponseWriter, cib_data string) bool {
+func handleApiNodesV2(w http.ResponseWriter, r *http.Request, cib_data string) bool {
 	fmt.Printf("handleApiNodesV2")
 	return true
 }
