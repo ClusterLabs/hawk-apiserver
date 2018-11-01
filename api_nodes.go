4a0b9dde2cb20a568b0e147457478de6937cdefa
liangxin1300 <XLiang@suse.com>
2018-11-01 15:27:32 +0800

    dev: Give pretty print by default, controlled by request header

--- api_nodes.go
+++ api_nodes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	log "github.com/sirupsen/logrus"
@@ -46,7 +45,7 @@ func handleApiNodes(w http.ResponseWriter, r *http.Request, cib_data string) boo
 		cib.Configuration.Nodes.URLIndex = index
 	}
 
-	jsonData, jsonError := json.Marshal(&cib)
+	jsonData, jsonError := MarshalOut(r, &cib)
 	if jsonError != nil {
 		log.Error(jsonError)
 		return false
