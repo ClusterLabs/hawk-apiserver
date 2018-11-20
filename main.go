1fb12662c06404cbb123a57a3906cb5ce856ddd6
liangxin1300 <XLiang@suse.com>
2018-11-20 19:48:45 +0800

    dev: implement /configuration/fencing

--- main.go
+++ main.go
@@ -294,6 +294,10 @@ func (handler *routeHandler) serveAPI(w http.ResponseWriter, r *http.Request, ro
 		if match {
 			return handleApiAcls(w, r, handler.cib.Get())
 		}
+		match, _ = regexp.MatchString(prefix + "fencing(/?|/[a-zA-Z0-9]+/?)$", r.URL.Path)
+		if match {
+			return handleApiFencing(w, r, handler.cib.Get())
+		}
 		if strings.HasPrefix(r.URL.Path, prefix+"cib.xml") {
 			xmldoc := handler.cib.Get()
 			w.Header().Set("Content-Type", "application/xml")
