a74a1796184194b66f206cc87af066e90d0297aa
liangxin1300 <XLiang@suse.com>
2018-11-20 15:46:13 +0800

    dev: implement /configuration/op_defaults

--- api_common.go
+++ api_common.go
@@ -54,6 +54,8 @@ func (c *Cib) MarshalJSON() ([]byte, error) {
 		}
 	case "rsc_defaults":
 		struct_interface = c.Configuration.RscDefaults
+	case "op_defaults":
+		struct_interface = c.Configuration.OpDefaults
 	}
 
 	jsonValue, err := json.Marshal(struct_interface)
--- api_defaults.go
+++ api_defaults.go
@@ -29,3 +29,26 @@ func handleApiRscDefaults(w http.ResponseWriter, r *http.Request, cib_data strin
 	io.WriteString(w, string(jsonData)+"\n")
 	return true
 }
+
+func handleApiOpDefaults(w http.ResponseWriter, r *http.Request, cib_data string) bool {
+	// parse xml into Cib struct
+	var cib Cib
+	err := xml.Unmarshal([]byte(cib_data), &cib)
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+
+	cib.Configuration.URLType = "op_defaults"
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonData, jsonError := MarshalOut(r, &cib)
+	if jsonError != nil {
+		log.Error(jsonError)
+		return false
+	}
+
+	io.WriteString(w, string(jsonData)+"\n")
+	return true
+}
