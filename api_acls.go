c0b5fe7ba4672cd03de148e2635c3367501b8dbb
liangxin1300 <XLiang@suse.com>
2019-02-13 15:22:34 +0800

    dev: api_acls: Change the interface define

--- api_acls.go
+++ api_acls.go
@@ -1,31 +1,5 @@
 package main
 
-func handleConfigAcls(urllist []string, cib Cib) bool {
-
-	if len(urllist) == 4 {
-		cib.Configuration.Acls.URLType = "all"
-	} else {
-		aclId := urllist[4]
-
-		mapIdType := make(map[string]TypeIndex)
-		for ti, titem := range cib.Configuration.Acls.AclTarget {
-			mapIdType[titem.Id] = TypeIndex{"target", ti}
-		}
-		for gi, gitem := range cib.Configuration.Acls.AclGroup {
-			mapIdType[gitem.Id] = TypeIndex{"group", gi}
-		}
-		for ri, ritem := range cib.Configuration.Acls.AclRole {
-			mapIdType[ritem.Id] = TypeIndex{"role", ri}
-		}
-
-		val, ok := mapIdType[aclId]
-		if ok {
-			cib.Configuration.Acls.URLType = val.Type
-			cib.Configuration.Acls.URLIndex = val.Index
-		} else {
-			return false
-		}
-	}
-
-	return true
+func handleConfigAcls(urllist []string, cib *Cib) (bool, interface{}) {
+	return true, nil
 }
