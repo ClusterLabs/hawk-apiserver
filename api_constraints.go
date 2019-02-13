8496dab352a10d4f1c23f26fbfb0f20c10284bae
liangxin1300 <XLiang@suse.com>
2019-02-13 15:36:16 +0800

    dev: api_constraints: Rewrite api_constraints.go   Include these apis:   api/v1/configuration/constraints   api/v1/configuration/locations   api/v1/configuration/colocations   api/v1/configuration/orders

--- api_constraints.go
+++ api_constraints.go
@@ -1,31 +1,101 @@
 package main
 
-func handleConfigConstraints(urllist []string, cib Cib) bool {
+type SimpleConstraints struct {
+	Id   string `json:"id"`
+	Type string `json:"type"`
+}
 
-	if len(urllist) == 4 {
-		cib.Configuration.Constraints.URLType = "all"
-	} else {
-		consId := urllist[4]
+// Handle function for api/v1/configuration/constraints
+func handleConfigConstraints(urllist []string, cib *Cib) (bool, interface{}) {
+	constraints := make(map[string]string)
+	for _, item := range cib.Configuration.Constraints.RscLocation {
+		constraints[item.Id] = "location"
+	}
+	for _, item := range cib.Configuration.Constraints.RscColocation {
+		constraints[item.Id] = "colocation"
+	}
+	for _, item := range cib.Configuration.Constraints.RscOrder {
+		constraints[item.Id] = "order"
+	}
 
-		mapIdType := make(map[string]TypeIndex)
-		for li, litem := range cib.Configuration.Constraints.RscLocation {
-			mapIdType[litem.Id] = TypeIndex{"location", li}
-		}
-		for ci, citem := range cib.Configuration.Constraints.RscColocation {
-			mapIdType[citem.Id] = TypeIndex{"colocation", ci}
-		}
-		for oi, oitem := range cib.Configuration.Constraints.RscOrder {
-			mapIdType[oitem.Id] = TypeIndex{"order", oi}
-		}
+	simple_cons := make([]SimpleConstraints, 0)
+	for key, value := range constraints {
+		simple_cons = append(simple_cons, SimpleConstraints{Id: key, Type: value})
+	}
 
-		val, ok := mapIdType[consId]
-		if ok {
-			cib.Configuration.Constraints.URLType = val.Type
-			cib.Configuration.Constraints.URLIndex = val.Index
-		} else {
-			return false
+	if len(urllist) == 5 {
+		switch constraints[urllist[4]] {
+		case "location":
+			return handleConfigLocation(urllist, cib)
+		case "colocation":
+			return handleConfigColocation(urllist, cib)
+		case "order":
+			return handleConfigOrder(urllist, cib)
 		}
 	}
 
-	return true
+	return true, simple_cons
+}
+
+// Handle function for api/v1/configuration/locations
+func handleConfigLocation(urllist []string, cib *Cib) (bool, interface{}) {
+	location_data := cib.Configuration.Constraints.RscLocation
+	if location_data == nil {
+		return true, nil
+	}
+
+	locationId := ""
+	if len(urllist) == 5 {
+		// api/v1/configuration/locations/:id
+		locationId = urllist[4]
+		for _, item := range location_data {
+			if item.Id == locationId {
+				return true, item
+			}
+		}
+	}
+
+	return true, location_data
+}
+
+// Handle function for api/v1/configuration/colocations
+func handleConfigColocation(urllist []string, cib *Cib) (bool, interface{}) {
+	colocation_data := cib.Configuration.Constraints.RscColocation
+	if colocation_data == nil {
+		return true, nil
+	}
+
+	colocationId := ""
+	if len(urllist) == 5 {
+		// api/v1/configuration/colocations/:id
+		colocationId = urllist[4]
+		for _, item := range colocation_data {
+			if item.Id == colocationId {
+				return true, item
+			}
+		}
+	}
+
+	return true, colocation_data
+}
+
+// Handle function for api/v1/configuration/orders
+func handleConfigOrder(urllist []string, cib *Cib) (bool, interface{}) {
+	order_data := cib.Configuration.Constraints.RscOrder
+	if order_data == nil {
+		return true, nil
+	}
+
+	orderId := ""
+	if len(urllist) == 5 {
+		// api/v1/configuration/orders/:id
+		orderId = urllist[4]
+		for _, item := range order_data {
+			if item.Id == orderId {
+				return true, item
+			}
+		}
+	}
+
+	return true, order_data
 }
