1e6f6a810b91cd2076cdd63a03ecb26e058f7bb8
liangxin1300 <XLiang@suse.com>
2019-02-13 15:23:10 +0800

    dev: api_alerts: Change the interface define

--- api_alerts.go
+++ api_alerts.go
@@ -1,26 +1,5 @@
 package main
 
-func handleConfigAlerts(urllist []string, cib Cib) bool {
-
-	if len(urllist) == 4 {
-		cib.Configuration.Alerts.URLType = "all"
-	} else {
-		cib.Configuration.Alerts.URLType = "alert"
-
-		alertIndex := urllist[4]
-		var index int = -1
-		for i, item := range cib.Configuration.Alerts.Alert {
-			if alertIndex == item.Id {
-				index = i
-				break
-			}
-		}
-		if index == -1 {
-			return false
-		}
-
-		cib.Configuration.Alerts.URLIndex = index
-	}
-
-	return true
+func handleConfigAlerts(urllist []string, cib *Cib) (bool, interface{}) {
+	return true, nil
 }
