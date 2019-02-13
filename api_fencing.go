a3bcc5f1b3b6786b878bb1f556934e65958f1528
liangxin1300 <XLiang@suse.com>
2019-02-13 15:36:54 +0800

    dev: api_fencing: Change the interface define

--- api_fencing.go
+++ api_fencing.go
@@ -1,26 +1,5 @@
 package main
 
-func handleConfigFencing(urllist []string, cib Cib) bool {
-
-	if len(urllist) == 4 {
-		cib.Configuration.FencingTopology.URLType = "all"
-	} else {
-		cib.Configuration.FencingTopology.URLType = "fence"
-
-		fenceIndex := urllist[4]
-		var index int = -1
-		for i, item := range cib.Configuration.FencingTopology.FencingLevel {
-			if fenceIndex == item.Id {
-				index = i
-				break
-			}
-		}
-		if index == -1 {
-			return false
-		}
-
-		cib.Configuration.FencingTopology.URLIndex = index
-	}
-
-	return true
+func handleConfigFencing(urllist []string, cib *Cib) (bool, interface{}) {
+	return true, nil
 }
