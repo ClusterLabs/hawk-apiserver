bad8e217813dcc02359af1360b291d5ff6cf225d
liangxin1300 <XLiang@suse.com>
2019-02-13 15:56:46 +0800

    dev: api_tags: Change the interface define

--- api_tags.go
+++ api_tags.go
@@ -1,26 +1,5 @@
 package main
 
-func handleConfigTags(urllist []string, cib Cib) bool {
-
-	if len(urllist) == 4 {
-		cib.Configuration.Tags.URLType = "all"
-	} else {
-		cib.Configuration.Tags.URLType = "tag"
-
-		tagIndex := urllist[4]
-		var index int = -1
-		for i, item := range cib.Configuration.Tags.Tag {
-			if tagIndex == item.Id {
-				index = i
-				break
-			}
-		}
-		if index == -1 {
-			return false
-		}
-
-		cib.Configuration.Tags.URLIndex = index
-	}
-
-	return true
+func handleConfigTags(urllist []string, cib *Cib) (bool, interface{}) {
+	return true, nil
 }
