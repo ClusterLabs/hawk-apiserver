8431d1ae3dddae4c5b6579e4d88b0e5f092ff3a9
Kristoffer Grönlund <krig@koru.se>
2019-03-07 17:01:14 +0100

    test: Use testify in main_test

--- main_test.go
+++ main_test.go
@@ -2,31 +2,26 @@ package main
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConfigParse(t *testing.T) {
 	config := Config{}
 	parseConfigFile("./config.json.example", &config)
-	if config.Port != 7630 {
-		t.Fatal("expected 7630, got ", config.Port)
-	}
+	assert.Equal(t, config.Port, 7630, "Port should be 7630")
 }
 
 func TestRouteHandler(t *testing.T) {
 	config := Config{}
 	parseConfigFile("./config.json.example", &config)
 	routeHandler := newRouteHandler(&config)
-	if routeHandler == nil {
-		t.Fatal("Failed to create a route handler")
-	}
+	assert.NotNil(t, routeHandler)
 
 	for route := range config.Route {
 		if config.Route[route].Handler == "proxy" {
 			p1 := routeHandler.proxyForRoute(&config.Route[route])
 			p2 := routeHandler.proxyForRoute(&config.Route[route])
-			if p1 != p2 {
-				t.Fatalf("route cache returns inconsistent results for route #%v", route)
-			}
+			assert.Equal(t, p1, p2, "route cache returns inconsistent results")
 		}
 	}
 }
