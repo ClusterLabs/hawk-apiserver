e91f9f6b48c10794a01bf008b55af1ce0958b483
dmaiocchi <dmaiocchi@suse.com>
2020-11-03 11:48:44 +0100

    Add https hardening chipers and ssl protocol

--- server/redirect.go
+++ server/redirect.go
@@ -41,6 +41,17 @@ func ListenAndServeWithRedirect(addr string, handler http.Handler, cert string, 
 		Handler: &httpRedirectHandler{
 			handler: handler,
 		},
+		config := &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		}
 	}
 	srv.SetKeepAlivesEnabled(true)
 	srv.Serve(listener)
