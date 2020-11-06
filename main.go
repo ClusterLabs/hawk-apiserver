23ecdc804a42abeba6ff8c811ccd363f12100973
dmaiocchi <dmaiocchi@suse.com>
2020-11-06 18:17:01 +0100

    make comments

--- main.go
+++ main.go
@@ -12,11 +12,14 @@ import (
 var version = "was not built correctly"
 
 func main() {
+	// initialize configuration, which handle the different routes
 	config := internal.InitConfig(version)
 	routehandler := internal.NewRouteHandler(&config)
+	// binds to pacemaker-go and serve CIB async info
 	routehandler.Cib.Start()
 
 	log.Infof("Listening to https://%s:%d\n", config.Listen, config.Port)
 	//TODO: this function should return errors
+	// an https server with a reverse proxy. http is redirected to https
 	server.ListenAndServeWithRedirect(fmt.Sprintf("%s:%d", config.Listen, config.Port), routehandler, config.Cert, config.Key)
 }
